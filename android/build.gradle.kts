94238773db0e0394f549b30de9e5922c4a11389f
4zyz4 <Z_4zyz4@outlook.com>
2026-07-07 17:58:06 +0800

    1.3.1

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 36
         versionCode = 6
-        versionName = "1.3.0"
+        versionName = "1.3.1"
     }
 
     buildTypes {
