45e76980fab6f768f0456ff12f2aad0ab329d647
4zyz4 <Z_4zyz4@outlook.com>
2026-09-05 17:17:52 +0800

    chore: bump version to 4.0.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         minSdk = 26
         //noinspection OldTargetApi
         targetSdk = 36
-        versionCode = 3101
-        versionName = "3.1.0TEST"
+        versionCode = 4000
+        versionName = "4.0.0"
     }
 
     buildTypes {
